6df5be646330d07d5aa1900776d15f9f945f4f5f
parkmyengbae <gambler1541@gmail.com>
2022-11-11 16:18:44 +0900

    async, await를 이용한 비동기처리

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,57 @@
 import { fetchNewsList, fetchJobsList, fetchAskList, fetchUserInfo, fetchItemInfo, fetchList } from '../api/index'
 
 export default {
-    FETCH_NEWS({ commit }) {
-        return fetchNewsList()
-            .then(res => {
-                commit('SET_NEWS', res.data);
-                return res;
-            })
-            .catch(e => {
-                console.log(e);
-            })
+    // promise
+    // FETCH_NEWS({ commit }) {
+    //     return fetchNewsList()
+    //         .then(res => {
+    //             commit('SET_NEWS', res.data);
+    //             return res;
+    //         })
+    //         .catch(e => {
+    //             console.log(e);
+    //         })
+    // },
+
+    // async
+    async FETCH_NEWS({ commit }) {
+        const response = await fetchNewsList();
+        commit('SET_NEWS', response.data);
+        return response;
     },
-    FETCH_JOBS({ commit }) {
-        return fetchJobsList()
-            .then(res => {
-                commit('SET_JOBS', res.data);
-                return res;
-            })
-            .catch(e => {
-                console.log(e);
-            })
+
+    // FETCH_JOBS({ commit }) {
+    //     return fetchJobsList()
+    //         .then(res => {
+    //             commit('SET_JOBS', res.data);
+    //             return res;
+    //         })
+    //         .catch(e => {
+    //             console.log(e);
+    //         })
+    // },
+    async FETCH_JOBS({ commit }){
+        const response = await fetchJobsList();
+        commit('SET_JOBS', response.data);
+        return response;
     },
-    FETCH_ASK({ commit }) {
-        return fetchAskList()
-            .then(res => {
-                commit('SET_ASK', res.data);
-                return res;
-            })
-            .catch(e => {
-                console.log(e);
-            })
+
+    // FETCH_ASK({ commit }) {
+    //     return fetchAskList()
+    //         .then(res => {
+    //             commit('SET_ASK', res.data);
+    //             return res;
+    //         })
+    //         .catch(e => {
+    //             console.log(e);
+    //         })
+    // },
+    async FETCH_ASK({ commit }){
+        const response = await fetchAskList();
+        commit('SET_ASK', response.data);
+        return response;
     },
+
     FETCH_USER({ commit }, name){
         return fetchUserInfo(name)
             .then(res => {
@@ -49,12 +70,21 @@ export default {
                 console.log(e);
             })
     },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-            .then( ({data}) => {
-                commit('SET_LIST', data);
-                return data;
-            })
-            .catch(e => console.log(e))
-    },
+    // FETCH_LIST({ commit }, pageName) {
+    //     return fetchList(pageName)
+    //         .then( ({data}) => {
+    //             commit('SET_LIST', data);
+    //             return data;
+    //         })
+    //         .catch(e => console.log(e))
+    // },
+    async FETCH_LIST({ commit }, pageName){
+        try {
+            const response = await fetchList(pageName);
+            commit('SET_LIST', response.data);
+            return response;            
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
